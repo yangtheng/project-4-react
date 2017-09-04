a036b04c1197cb7c51e369f8b2768ba627813701
Dominic Phua <dominikphua@gmail.com>
2017-09-04 18:38:45 +0800

    Added Cloudinary

--- src/EditBlogPage.js
+++ src/EditBlogPage.js
@@ -3,8 +3,12 @@ import CoverPhotoEditPage from './CoverPhotoEditPage'
 // import ActivityBody from './ActivityBody'
 import {Button, Modal, PanelGroup, Panel, Glyphicon} from 'react-bootstrap'
 import './App.css'
+import Dropzone from 'react-dropzone'
+import sha1 from 'sha1'
+import superagent from 'superagent'
 
 class EditBlogPage extends Component {
+
   constructor () {
     super()
 
@@ -14,10 +18,79 @@ class EditBlogPage extends Component {
       activities: [],
       newTitle: '',
       newContent: '',
-      newLocation: ''
+      newLocation: '',
+      images: []
     }
   }
+
+  uploadFile (files) {
+    console.log('uploadFile: ')
+    const image = files[0]
+
+    const cloudName = 'dominikphua'
+
+    const url = 'https://api.cloudinary.com/v1_1/' + cloudName + '/image/upload'
+
+    const timestamp = Date.now()/1000
+
+    const uploadPreset = 'sqspzusi'
+
+    const paramsStr = 'timestamp=' + timestamp + '&upload_preset=' + uploadPreset + 'weRc9kcELrJBhBeqL0Zi6OQhVew'
+
+    const signature = sha1(paramsStr)
+    const params = {
+      'api_key': '881529489275562',
+      'timestamp': timestamp,
+      'upload_preset': uploadPreset,
+      'signature': signature
+    }
+
+    let uploadRequest = superagent.post(url)
+    uploadRequest.attach('file', image)
+
+    Object.keys(params).forEach((key) => {
+      uploadRequest.field(key, params[key])
+    })
+
+    uploadRequest.end((err,resp) => {
+      if (err) {
+        alert(err)
+        return
+      }
+      console.log('UPLOAD COMPLETE: ' + JSON.stringify(resp.body))
+      const uploaded = resp.body
+
+      let updatedImages = Object.assign([], this.state.images)
+      updatedImages.push(uploaded.secure_url)
+
+      this.setState({
+        images: updatedImages
+      })
+    })
+  }
+
+  removeImage(event) {
+    event.preventDefault()
+    console.log('removeImage: '+ event.target.id)
+
+    let updatedImages = Object.assign([], this.state.images)
+    updatedImages.splice(event.target.id, 1)
+
+    this.setState({
+      images: updatedImages
+    })
+  }
+
   render () {
+    const list = this.state.images.map((image, i) => {
+      return (
+        <li key={i}>
+          <img style={{width:72}} src={image} />
+          <br /><a id={i} onClick={this.removeImage.bind(this)} href='#'>Remove</a>
+        </li>
+      )
+    })
+
     let addActivityForm = (
       <Modal show={this.state.addingActivity} onHide={() => this.closeAddActivityWindow()}>
         <Modal.Header>
@@ -37,6 +110,14 @@ class EditBlogPage extends Component {
         <Modal.Body>
           <label>Content</label>
           <textarea className='form-control' value={this.state.newContent} rows='10' onChange={(e) => this.handleChange(e, 'newContent')} />
+        </Modal.Body>
+
+        <Modal.Body>
+          <label>Photo</label>
+          <Dropzone onDrop={this.uploadFile.bind(this)}/>
+          <ol>
+            { list }
+          </ol>
         </Modal.Body>
 
         <Modal.Footer>
@@ -122,15 +203,18 @@ class EditBlogPage extends Component {
     const newActivity = {
       title: this.state.newTitle,
       content: this.state.newContent,
-      location: this.state.newLocation
+      location: this.state.newLocation,
+      photos: this.state.images
     }
-    this.setState({
-      addingActivity: false,
-      activities: this.state.activities.concat(newActivity),
-      newTitle: '',
-      newContent: '',
-      newLocation: ''
-    })
+    console.log (newActivity)
+
+    // this.setState({
+    //   addingActivity: false,
+    //   activities: this.state.activities.concat(newActivity),
+    //   newTitle: '',
+    //   newContent: '',
+    //   newLocation: ''
+    // })
   }
 }
 
