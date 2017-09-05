bdd06c12a2d02a3f24bbe53ad0ad8e27131d5c97
Rebecca Tay <rebeccatay92@gmail.com>
2017-09-05 10:29:51 +0800

    changed font color and shadow in itineraryBody

--- src/ItineraryBody.js
+++ src/ItineraryBody.js
@@ -18,9 +18,9 @@ class ItineraryBody extends Component {
   render () {
     return (
       <Panel className="col-sm-6" style={{backgroundImage: 'url(' + this.state.itinerary.bannerUrl + ')', backgroundSize: 'cover', height: '30vh', position: 'relative', width: '50%', float: 'left'}}>
-        <h3>Title: {this.state.itinerary.title}</h3>
-        <h3>Country: {this.state.itinerary.country}</h3>
-        <h3>Days: {this.state.itinerary.days}</h3>
+        <h3 style={{color: 'white', fontWeight: 'bold', textShadow: 'black 0.1em 0.1em 0.2em'}}>Title: {this.state.itinerary.title}</h3>
+        <h3 style={{color: 'white', fontWeight: 'bold', textShadow: 'black 0.1em 0.1em 0.2em'}}>Country: {this.state.itinerary.country}</h3>
+        <h3 style={{color: 'white', fontWeight: 'bold', textShadow: 'black 0.1em 0.1em 0.2em'}}>Days: {this.state.itinerary.days}</h3>
         <Button onClick={() => this.deleteItinerary()} bsStyle='danger' style={{float: 'right', marginRight: '3vh'}}>Delete</Button>
         <Link to={'/edit-blogpage/' + this.state.itinerary_id}><Button bsStyle='info' style={{float: 'right', marginRight: '3vh'}}>Edit this itinerary</Button></Link>
       </Panel>
