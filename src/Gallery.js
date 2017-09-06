5870da4238edce728cf0b0973172459f8bc8cea6
Rebecca Tay <rebeccatay92@gmail.com>
2017-09-06 14:42:34 +0800

    routing fix

--- src/Gallery.js
+++ src/Gallery.js
@@ -13,7 +13,7 @@ class Gallery extends Component {
   render() {
     console.log(this.props.images)
     const pics = this.props.images.map((child, i) => {
-      var url = 'https://project-4-backend.herokuapp.com/blog/' + child.id
+      var url = '/blog/' + child.id
       var id = child.id
       return(
         <Link to={url}><img className='grid-item' src={child.bannerUrl} key={i} /></Link>
